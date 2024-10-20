e7ae73109007901bc93b7ad25253e71f8f5a4392
Oakley <kiran.nowak@gmail>
2024-10-20 15:33:09 +0000

    compare with std::unordered_map, ankerl::unordered_dense::map

--- samples/test-ExpiringLruCache.cc
+++ samples/test-ExpiringLruCache.cc
@@ -2,8 +2,17 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <unordered_map>
 #include "contrib/cache_profiling.h"
 #include "contrib/expiring_lru_cache.h"
+
+#if defined(__has_include) && __has_include(<tsl/robin_map.h>)
+
+#include <tsl/robin_map.h> // tsl::robin_map
+
+#define HAS_ROBIN_MAP
+
+#endif
 
 class CacheBenchmark_Generic : public CacheBenchmark<std::string, int> {
  public:
@@ -24,6 +33,23 @@ class CacheBenchmark_Generic : public CacheBenchmark<std::string, int> {
   }
 };
 
+static thread_local std::unordered_map<std::string, int> cache1_;
+class Bench_threadlocal_StdUnorderedMap : public CacheBenchmark_Generic {
+ public:
+  Bench_threadlocal_StdUnorderedMap() : CacheBenchmark_Generic() {}
+
+  virtual bool get_only(const std::string &key) override
+  {
+    return cache1_.find(key) != cache1_.end();
+  }
+
+  virtual bool set_only(const std::string &key, const int &value) override
+  {
+    cache1_.emplace(key, value);
+    return true;
+  }
+};
+
 static thread_local ankerl::unordered_dense::map<std::string, int> cache2_;
 class Bench_threadlocal_UnorderedMap : public CacheBenchmark_Generic {
  public:
@@ -41,38 +67,73 @@ class Bench_threadlocal_UnorderedMap : public CacheBenchmark_Generic {
   }
 };
 
-static thread_local ExpiringLruCache<std::string, int> cache_(300000, 30);
+#ifdef HAS_ROBIN_MAP
+static thread_local tsl::robin_map<std::string, int> cache3_;
+class Bench_threadlocal_RobinMap : public CacheBenchmark_Generic {
+ public:
+  Bench_threadlocal_RobinMap() : CacheBenchmark_Generic() {}
+
+  virtual bool get_only(const std::string &key) override
+  {
+    return cache3_.find(key) != cache3_.end();
+  }
+
+  virtual bool set_only(const std::string &key, const int &value) override
+  {
+    cache3_.emplace(key, value);
+    return true;
+  }
+};
+#endif
+
+static thread_local ExpiringLruCache<std::string, int> cache4_(300000, 30);
 class Bench_threadlocal_LRU : public CacheBenchmark_Generic {
  public:
   Bench_threadlocal_LRU() : CacheBenchmark_Generic() {}
 
   virtual bool get_only(const std::string &key) override
   {
-    return cache_.find(key) != cache_.end();
+    return cache4_.find(key) != cache4_.end();
   }
 
   virtual bool set_only(const std::string &key, const int &value) override
   {
-    cache_.emplace(key, value);
+    cache4_.emplace(key, value);
     return true;
   }
 };
 
 int main()
 {
-  if (1) {
+  {
     Bench_threadlocal_LRU bench;
     bench.Run("ExpiringLruCache", 3, 12);
     bench.Run("ExpiringLruCache", 3, 0.05, 12);
     bench.Run("ExpiringLruCache", 3, 0.05, 16.0, 0.1, 12);
   }
 
-  if (1) {
+  {
+    Bench_threadlocal_StdUnorderedMap bench;
+    bench.Run("std::unordered_map", 3, 12);
+    bench.Run("std::unordered_map", 3, 0.05, 12);
+    bench.Run("std::unordered_map", 3, 0.05, 16.0, 0.1, 12);
+  }
+
+  {
     Bench_threadlocal_UnorderedMap bench;
     bench.Run("unordered_dense::map", 3, 12);
     bench.Run("unordered_dense::map", 3, 0.05, 12);
     bench.Run("unordered_dense::map", 3, 0.05, 16.0, 0.1, 12);
   }
+
+#ifdef HAS_ROBIN_MAP
+  {
+    Bench_threadlocal_RobinMap bench;
+    bench.Run("robin_map", 3, 12);
+    bench.Run("robin_map", 3, 0.05, 12);
+    bench.Run("robin_map", 3, 0.05, 16.0, 0.1, 12);
+  }
+#endif
 
   {
     auto map = ankerl::unordered_dense::map<int, std::string>();
